0bfef1ea6b87a30a76de4308cc77c612ecc2742d
mobbexLuis <luis.scarrone@mobbex.com>
2023-04-21 10:40:15 -0300

    delete sdk classes require

--- wp-travel-mobbex.php
+++ wp-travel-mobbex.php
@@ -20,13 +20,8 @@ define('WPT_MOBBEX_VERSION', '1.0.0');
 define('WPT_MOBBEX_PATH', plugin_dir_path(__FILE__));
 define('WPT_MOBBEX_URL', plugin_dir_url(__FILE__));
 
+// require Sdk classes
 require_once 'vendor/autoload.php';
-
-// Include necesary classes from Sdk
-require_once WPT_MOBBEX_PATH . 'vendor/mobbexco/php-plugins-sdk/src/Api.php';
-require_once WPT_MOBBEX_PATH . 'vendor/mobbexco/php-plugins-sdk/src/Platform.php';
-require_once WPT_MOBBEX_PATH . 'vendor/mobbexco/php-plugins-sdk/src/Exception.php';
-require_once WPT_MOBBEX_PATH . 'vendor/mobbexco/php-plugins-sdk/src/Modules/Checkout.php';
 
 // Include helpers
 require_once WPT_MOBBEX_PATH . 'includes/helper/class-booking-helper.php';
