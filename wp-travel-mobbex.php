855d854c9c9300736cc61a7e2d6c27587e64707e
mobbexLuis <luis.scarrone@mobbex.com>
2023-07-12 13:28:15 -0300

    Changes requested in #4

--- wp-travel-mobbex.php
+++ wp-travel-mobbex.php
@@ -21,7 +21,7 @@ define('WPT_MOBBEX_PATH', plugin_dir_path(__FILE__));
 define('WPT_MOBBEX_URL', plugin_dir_url(__FILE__));
 
 // require Sdk classes
-require_once __DIR__ . 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Include helpers
 require_once WPT_MOBBEX_PATH . 'includes/helper/class-booking-helper.php';
